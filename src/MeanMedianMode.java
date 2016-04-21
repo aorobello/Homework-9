d44204550d2b730c4827c97d423a60090f673de0
aorobello <oroba32@mail.sunysuffolk.edu>
2016-04-21 15:14:47 -0400

    Update MeanMedianMode.java
    added getMedian method

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -13,7 +13,28 @@ public class MeanMedianMode {
 	}
 
 	public static double getMedian(double[] numberList) {
+		int factor = numberList.length - 1;
+		double[] first = new double[(int) ((double) factor / 2)];
+		double[] last = new double[first.length];
+		double[] middleNumbers = new double[1];
 
+		for (int i = 0; i < first.length; i++) {
+			first[i] = numberList[i];
+		}
+		for (int i = numberList.length; i > last.length; i--) {
+			last[i] = numberList[i];
+		}
+		for (int i = 0; i <= numberList.length; i++) {
+			if (numberList[i] != first[i] || numberList[i] != last[i])
+				middleNumbers[i] = numberList[i];
+		}
+		if (numberList.length % 2 == 0) {
+			double total = middleNumbers[0] + middleNumbers[1];
+			return total / 2;
+		}
+		else {
+			return middleNumbers[0];
+		}
 	}
 
 	public static double getMode(double[] numberList) {
