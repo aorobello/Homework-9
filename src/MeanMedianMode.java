787a95ab3568a7844042666e8473b165dd06c3be
aorobello <oroba32@mail.sunysuffolk.edu>
2016-04-21 15:15:25 -0400

    Update MeanMedianMode.java
    Added getMode method

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -38,8 +38,23 @@ public class MeanMedianMode {
 	}
 
 	public static double getMode(double[] numberList) {
+		HashMap<Double, Double> freqs = new HashMap<Double, Double>();
 
+		for (double d : numberList) {
+			Double freq = freqs.get(d);
+			freqs.put(d, (freq == null ? 1 : freq + 1));
+		}
+		double mode = 0;
+		double maxFreq = 0;
 
+		for (Map.Entry<Double, Double> entry : freqs.entrySet()) {
+			double freq = entry.getValue();
+			if (freq > maxFreq) {
+				maxFreq = freq;
+				mode = entry.getKey();
+			}
+		}
+		return mode;
 	}
 
 }
