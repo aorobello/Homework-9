a1dd920f84ef8a1ce9b0a92cc0cce983afb853c4
aorobello <oroba32@mail.sunysuffolk.edu>
2016-04-21 15:13:50 -0400

    Update MeanMedianMode.java
    Added getMean Method

--- src/MeanMedianMode.java
+++ src/MeanMedianMode.java
@@ -4,7 +4,12 @@ import java.util.Map;
 public class MeanMedianMode {
 
 	public static double getMean(double[] numberList) {
+		double total = 0;
 
+		for (double d : numberList) {
+			total += d;
+		}
+		return total / (numberList.length);
 	}
 
 	public static double getMedian(double[] numberList) {
